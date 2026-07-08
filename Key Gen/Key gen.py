7a75e78800be7c12593d6fc6560bb068917725ad
CarelessEevee <138996170+eeveeyynaerlge@users.noreply.github.com>
2026-07-08 22:30:27 +0100

    Refactor key generation code structure

--- Key Gen/Key gen.py
+++ Key Gen/Key gen.py
@@ -1,11 +1,10 @@
 import random,pyperclip,time
+def generate_random_windows_key(length):
+    characters = 'BCDFGHJKMNPQRTVWXY23456789'
+    key = ''.join(random.choice(characters) for _ in range(length))
+    key_with_hyphens = '-'.join([key[i:i+5] for i in range(0, length, 5)])
+    return key_with_hyphens
 while True:
- def generate_random_windows_key(length):
-     characters = 'BCDFGHJKMNPQRTVWXY23456789'
-     key = ''.join(random.choice(characters) for _ in range(length))
-     key_with_hyphens = '-'.join([key[i:i+5] for i in range(0, length, 5)])
-     return key_with_hyphens
-
  random_key = generate_random_windows_key(25)
  print(random_key)
  pyperclip.copy(random_key)
